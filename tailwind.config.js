cfc45bd19cedcbfeb594bf41d86c3e1d267f993c
Frank DeRosa <surrealdreams@gmail.com>
2024-04-15 18:10:16 -0400

    Add Tailwind resources for CSS styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+    "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+  ],
   theme: {
     extend: {},
   },
